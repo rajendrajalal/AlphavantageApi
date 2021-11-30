1dc34c3d8ff5aa94182b7029c2e2550ebfe99db0
rajendra jalal <rajendra.jalal@gmail.com>
2021-11-30 19:39:17 +0530

    Update settings.py

--- AlphavantageApi/settings.py
+++ AlphavantageApi/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
 SECRET_KEY = env("SECRET_KEY")
+RAPIDAPIKEY=env("RAPIDAPIKEY")
 
 
 
